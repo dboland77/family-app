946ef3fa9a999caa89d8b17d3c6c12f1595b161a
David Boland <dboland77@gmail.com>
2021-01-13 11:20:23 +0000

    added get and post messages to firebase functions

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,9 +1,50 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello world!");
- });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello world!");
+});
+
+exports.getMessages = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("messages")
+    .get()
+    .then((data) => {
+      let messages = [];
+      data.forEach((doc) => {
+        messages.push(doc.data());
+      });
+      return res.json(messages);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createMessage = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method not allowed" });
+  }
+  const newMessage = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("messages")
+    .add(newMessage)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
